316585172e0bc06e1b3e1a26894093acc1a8d901
1twalters01 <1twalters01@gmail.com>
2023-04-02 17:51:40 +0100

    javascript finished

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Navbar
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
@@ -7,4 +8,29 @@ var currentScrollPos = window.pageYOffset;
     document.getElementById("navbar").style.top = "-50px";
   }
   prevScrollpos = currentScrollPos;
+}
+
+// Scrolling
+const navHome = document.getElementById('navHome')
+const navStack = document.getElementById('navStack')
+const navCurrent = document.getElementById('navCurrent')
+const navOther = document.getElementById('navOther')
+
+navHome.addEventListener("click", homeFunction)
+navStack.addEventListener("click", stackFunction)
+navCurrent.addEventListener("click", currentFunction)
+navOther.addEventListener("click", otherFunction)
+
+function homeFunction(){
+  document.getElementById("hero").scrollIntoView()
+}
+function stackFunction(){
+  document.getElementById("stack").scrollIntoView()
+}
+function currentFunction(){
+  document.getElementById("current").scrollIntoView()
+}
+function otherFunction(){
+  console.log('hi')
+  document.getElementById("other").scrollIntoView()
 }
